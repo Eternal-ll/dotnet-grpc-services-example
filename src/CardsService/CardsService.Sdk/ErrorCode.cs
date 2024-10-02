9bb46296276a3cf8a83010e69102a9d315307743
Eternal-ll <aigiz.iskuzhin@yandex.ru>
2024-10-02 20:42:09 +0500

    Добавил доменные коды ошибок и исключения

---
+++ src/CardsService/CardsService.Sdk/ErrorCode.cs
@@ -0,0 +1,21 @@
+﻿namespace CardsService.Sdk
+{
+    /// <summary>
+    /// Error codes for CardsService domain
+    /// </summary>
+    public enum ErrorCode : int
+    {
+        /// <summary>
+        /// Unknown internal error
+        /// </summary>
+        Unknown,
+        /// <summary>
+        /// Card type not found
+        /// </summary>
+        CardTypeNotFound,
+        /// <summary>
+        /// Card not found
+        /// </summary>
+        CardNotFound,
+    }
+}
---
+++ src/CardsService/CardsService.Sdk/Exceptions/ServiceException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardsService.Sdk.Exceptions
+{
+    /// <summary>
+    /// CardsService domain exception
+    /// </summary>
+    public class ServiceException : Exception
+    {
+        /// <summary>
+        /// Error code
+        /// </summary>
+        public ErrorCode ErrorCode { get; private set; }
+        public ServiceException(ErrorCode errorCode)
+        {
+            ErrorCode = errorCode;
+        }
+    }
+}
