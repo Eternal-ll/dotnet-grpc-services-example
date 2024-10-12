80922e3196bc0ea5b7204189ea9d988582f52fd4
Eternal-ll <aigiz.iskuzhin@yandex.ru>
2024-10-12 21:09:51 +0500

    Опрелил глобально cards-service entities&dto

---
+++ src/CardsService/CardsService.Api/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using CardDto = CardsService.Sdk.Card;
+global using CardEntity = CardsService.Database.Entities.Card;
+global using CardTypeDto = CardsService.Sdk.CardType;
+global using CardTypeEntity = CardsService.Database.Entities.CardType;
