b2a2a4427bb8a96cc3a7d33a804ed9def425289a
Eternal-ll <aigiz.iskuzhin@yandex.ru>
2024-10-12 21:10:58 +0500

    Обновил работу с маппером в cards-service

--- src/CardsService/CardsService.Api/Infrastructure/Services/InMemoryCardsService.cs
+++ src/CardsService/CardsService.Api/Infrastructure/Services/InMemoryCardsService.cs
@@ -1,6 +1,7 @@
 ﻿using CardsService.Database.Context;
 using CardsService.Sdk;
 using CardsService.Sdk.Exceptions;
+using MapsterMapper;
 using Microsoft.EntityFrameworkCore;
 
 namespace CardsService.Api.Infrastructure.Services
@@ -8,53 +9,38 @@ namespace CardsService.Api.Infrastructure.Services
     public class InMemoryCardsService : ICardsService
     {
         private readonly CardsContext _context;
+        private readonly IMapper _mapper;
 
-        public InMemoryCardsService(CardsContext context)
+        public InMemoryCardsService(CardsContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
-        public async Task<Card> Add(AddCardRequest request, CancellationToken cancellationToken = default)
+        public async Task<CardDto> Add(AddCardRequest request, CancellationToken cancellationToken = default)
         {
             var cardType = await _context.CardTypes.FirstOrDefaultAsync(x => x.Id == request.CardTypeId, cancellationToken) ?? throw new ServiceException(ErrorCode.CardTypeNotFound);
-            var card = new Database.Entities.Card()
+            var card = new CardEntity()
             {
+                Sn = Guid.NewGuid().ToString().Replace("-", null)[..8].ToUpper(),
                 CardTypeId = request.CardTypeId,
-                Sn = Guid.NewGuid().ToString().Replace("-", null)[..8].ToUpper()
+                CardType = cardType
             };
-            var entry = _context.Cards.Add(card);
-            await _context.SaveChangesAsync();
-            return new Card()
-            {
-                Id = entry.Entity.Id,
-                Sn = card.Sn,
-                Type = new()
-                {
-                    Id = card.CardTypeId,
-                    Name = cardType.Name
-                }
-            };
+            _context.Cards.Add(card);
+            await _context.SaveChangesAsync(cancellationToken);
+            return _mapper.Map<CardDto>(card);
         }
 
-        public async Task<Card> GetById(GetCardByIdRequest request, CancellationToken cancellationToken = default)
+        public async Task<CardDto> GetById(GetCardByIdRequest request, CancellationToken cancellationToken = default)
         {
             var card = await _context.Cards
                 .Include(x => x.CardType)
                 .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken)
                 ?? throw new ServiceException(ErrorCode.CardNotFound);
-            return new Card()
-            {
-                Id = card.Id,
-                Sn = card.Sn,
-                Type = new CardType()
-                {
-                    Id = card.CardTypeId,
-                    Name = card.CardType.Name
-                }
-            };
+            return _mapper.Map<CardDto>(card);
         }
 
-        public async Task<Card[]> GetCards(GetCardsRequest request, CancellationToken cancellationToken = default)
+        public async Task<CardDto[]> GetCards(GetCardsRequest request, CancellationToken cancellationToken = default)
         {
             var data = await _context.Cards
                 .Include(x => x.CardType)
@@ -62,24 +48,15 @@ namespace CardsService.Api.Infrastructure.Services
                 .Take(request.Take)
                 .ToArrayAsync(cancellationToken);
             return data
-                .Select(x => new Card()
-                {
-                    Id = x.Id,
-                    Type = new()
-                    {
-                        Id = x.CardTypeId,
-                        Name = x.CardType.Name
-                    },
-                    Sn = x.Sn
-                })
+                .Select(x => _mapper.Map<CardDto>(x))
                 .ToArray();
         }
 
-        public async Task<CardType[]> GetCardTypes(CancellationToken cancellationToken = default)
+        public async Task<CardTypeDto[]> GetCardTypes(CancellationToken cancellationToken = default)
         {
             var data = await _context.CardTypes.ToArrayAsync(cancellationToken);
             return data
-                .Select(x => new CardType() { Id = x.Id, Name = x.Name })
+                .Select(x => _mapper.Map<CardTypeDto>(x))
                 .ToArray();
         }
     }
